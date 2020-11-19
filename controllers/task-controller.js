add76b76ace682cbd8f3bd9e6c342b1650262bf9
unknown <febyann.mathews@gmail.com>
2020-11-19 15:10:54 +0530

    changed aws update file

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -163,17 +163,26 @@ Task.findOneAndUpdate({tid:req.params.id},{$push : {attachments: req.body}}, {ne
 
 exports.addAWSAttachement = function(req,res) {
    // console.log("inside AWS attachement");
-    req.body.created_at = new Date().getTime();
+   console.log("inside AWS attachement");
+   console.log(req.body);
+   req.body.created_at = new Date().getTime();
 
-    Task.findOneAndUpdate({tid:req.params.id}, { $push: { attachments: req.body}}, { new: true }, (err, task) => {
-        if (err) {
-           // console.log(err);
-            return res.status(400).json(err);
-        }
+   Task.findOne({$and: [{tid:req.params.id}, {"attachments.name":req.body.name }]}, (err, task) => {
+       if (task?.tid == req.params.id) {
+           console.log("inside same task id");
+           console.log("inside same task id",task);
+           Task.findOneAndUpdate({$and: [{tid:req.params.id}, {"attachments.name":req.body.name }]},{$push : {"attachments.created_at": req.body.created_at}},{new:true},(err,task)=>{            
+          return res.json(task);
+       });
+   }
+       else {
+   Task.findOneAndUpdate({tid:req.params.id},{$push : {attachments: req.body}},{new:true},(err,task)=>{    
+       console.log(task)
+       return res.json(task);
+   });
+}
 
-        return res.json(task);
-    });
-
+});
 
 }
 
