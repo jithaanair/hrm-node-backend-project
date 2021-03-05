a7d1e873f3c9aee0b1d1f8a8a67186ce11a89795
unknown <febyann.mathews@gmail.com>
2021-03-05 09:55:03 +0530

    Job controller

--- controllers/job-controller.js
+++ controllers/job-controller.js
@@ -1,5 +1,5 @@
 var Job = require('../model/Job');
-
+exports.jobArray=[];
 exports.getJobs = function(req,res) {
     //console.log("inside getJobs");
     Job.find()
@@ -12,4 +12,17 @@ exports.getJobs = function(req,res) {
        res.json(jobs);
     });  
   
+}
+exports.findJob = function(jobname) {
+    Job.find({jname:jobname})
+    .select('_id')
+    .exec((err,jobs) => {
+        if(err){
+            console.log(err);
+            return res.status(400).json(err);
+        }
+        console.log(jobs[0]._id);
+        this.jobArray.push(jobs[0]._id);
+        return jobs;
+    });
 }
