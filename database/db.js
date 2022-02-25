001eaa18c6452158e53dfcbb5c65be6fdd88c9c8
jishnu-9946 <98869305+jishnu-9946@users.noreply.github.com>
2022-02-25 10:50:56 +0530

    updates

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 module.exports = {
     //db: 'mongodb://localhost:27017/startup'
     //db: 'mongodb://116.68.73.227:27017/startup'
-    db:'mongodb+srv://dbUser:dbUserPassword@cluster0.fl0oq.mongodb.net/startup'
+    db:'mongodb://127.0.0.1:27017/startup'
+    // db:'mongodb+srv://dbUser:dbUserPassword@cluster0.fl0oq.mongodb.net/startup'
   };
